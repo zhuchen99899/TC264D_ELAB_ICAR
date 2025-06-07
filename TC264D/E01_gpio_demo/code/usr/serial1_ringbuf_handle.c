1268c37898a1939c26387f3c4e9cdfc5e26a3f6b
387646983 <387646983@qq.com>
2025-06-08 01:48:50 +0800

    添加层次状态机 菜单UI的案例，并加入markdown说明

--- TC264D/E01_gpio_demo/code/usr/serial1_ringbuf_handle.c
+++ TC264D/E01_gpio_demo/code/usr/serial1_ringbuf_handle.c
@@ -12,160 +12,280 @@ ELAB_TAG("ringbuf_handle");
 
 
 
-typedef struct serial_hsm_s
-{
-sm_t supper;
-ringbuf_t *ringbuffer; //环形缓冲区
-uint8_t frame[256]; //数据帧
-uint16_t len; //剩余长度字节
-uint8_t checksum; //校验和字节
-}serial_hsm_t;
+/**
+ *  ├── HomeScreen
+    └── Menu
+      ├── DisplaySettings
+      │     ├── Brightness
+      │     └── Contrast
+      └── NetworkSettings
+            ├── WiFi
+            └── Ethernet
+ * 
+ */
 
-#define CHAR_SIG 1
 sm_t hsm;
-static sm_ret_t init(sm_t *me,sm_event_t const *e);
-static sm_ret_t top(sm_t *me,sm_event_t const *e);
-
-static sm_ret_t s0(sm_t *me,sm_event_t const *e); //s0
-static sm_ret_t s1(sm_t *me,sm_event_t const *e);//s1
-static sm_ret_t s2(sm_t *me,sm_event_t const *e);//s2
-static sm_ret_t s3(sm_t *me,sm_event_t const *e);//s3
-
-static sm_ret_t init(sm_t *me,sm_event_t const *e)
+enum
 {
-    elog_error("init");
-    (void) me; (void) *e;
-    return SM_TRAN(me, top);
+    SIG_MENU=0, //切到菜单
+    SIG_ENTER_DISPLAY, 
+    SIG_ENTER_BRIGHTNESS,
+    SIG_ENTER_WIFI,
+    SIG_DOWN, //同级别 向下菜单切换
+    SIG_UP,  //同级别 向上菜单切换
+    SIG_ESC
+};
+
+static sm_ret_t init(sm_t *me, sm_event_t const *e);
+static sm_ret_t begin_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t home_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t Menu_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t DisplaySettings_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t Brightness_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t Contrast_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t NetworkSettings_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t WIFI_state(sm_t *me, sm_event_t const *e);
+static sm_ret_t Ethernet_state(sm_t *me, sm_event_t const *e);
+
+
+static sm_ret_t init(sm_t *me, sm_event_t const *e)
+{
+    (void)me;(void)e;
+    return SM_TRAN(me, begin_state);
 }
-static sm_ret_t top(sm_t *me,sm_event_t const *e)
+static sm_ret_t begin_state(sm_t *me, sm_event_t const *e)
 {
 switch (e->sig)
-{
-case SM_ENTRY_SIG:
-    printf("top entry\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_EXIT_SIG:
-    printf("top exit\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_INIT_SIG:
-    printf("top init\n");
-    return SM_TRAN(me, s0);
-    /* code */
-}
-return SM_SUPER(me, hsm_top);
-}
-
-static sm_ret_t s0(sm_t *me,sm_event_t const *e)
-{
-
-switch (e->sig)
-{
-case SM_ENTRY_SIG:
-    printf("s0 entry\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_EXIT_SIG:
-    printf("s0 exit\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_INIT_SIG:
-    printf("s0 init\n");
-    return SM_TRAN(me, s2);
-    /* code */
-case CHAR_SIG:
-    printf("s0 char\n");
-    return SM_TRAN(me, s1);
-    /* code */
-}
-return SM_SUPER(me, hsm_top);
-}
-
-static sm_ret_t s1(sm_t *me,sm_event_t const *e)
-{
-
-switch (e->sig)
-{
-case SM_ENTRY_SIG:
-    printf("s1 entry\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_EXIT_SIG:
-    printf("s1 exit\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_INIT_SIG:
-    printf("s1 init\n");
-    return SM_TRAN(me, s3);
-    /* code */
-case CHAR_SIG:
-    printf("s1 char\n");
-    return SM_TRAN(me, s0);
-    /* code */
-}
-return SM_SUPER(me, hsm_top);
-}
-
-
-static sm_ret_t s2(sm_t *me,sm_event_t const *e)
-{
-
-switch (e->sig) {
+    {
     case SM_ENTRY_SIG:
-    printf("s2 entry\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_EXIT_SIG:
-    printf("s2 exit\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-}
-return SM_SUPER(me, s0);
+        printf("begine entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("begine  exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_INIT_SIG:
+        printf("begine init\r\n");
+        return SM_TRAN(me, home_state);
+    /* code */
+    }
+return SM_SUPER(me, hsm_top);
 
 }
 
-static sm_ret_t s3(sm_t *me,sm_event_t const*e)
+static sm_ret_t home_state(sm_t *me, sm_event_t const *e)
 {
-switch (e->sig) {
+    switch (e->sig)
+    {
     case SM_ENTRY_SIG:
-    printf("s3 entry\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
-case SM_EXIT_SIG:
-    printf("s3 exit\n");
-    return SM_HANDLED();  // 使用宏标记事件已处理
+        printf("home_State entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("home_State exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SIG_MENU:
+        printf("go Menu\r\n");
+        return SM_TRAN(me, Menu_state);
+    /* code */
+    }
+return SM_SUPER(me, begin_state);
+
 }
-return SM_SUPER(me, s1);
+
+//Menu 父状态，统一处理ESC返回
+static sm_ret_t Menu_state(sm_t *me, sm_event_t const *e)
+{
+    switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("Menu_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("Menu_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    // case SM_INIT_SIG:
+    //     printf("Menu_state init\r\n");
+    //     return SM_TRAN(me, DisplaySettings_state);
+    case SIG_ESC:
+        printf("back home\r\n");
+        return SM_TRAN(me,begin_state); // 统一 ESC 返回首页
+    case SIG_ENTER_DISPLAY:
+        printf("go DisplaySettings\r\n");
+        return SM_TRAN(me,DisplaySettings_state);
+    /* code */
+    }
+return SM_SUPER(me, home_state);
+
+
 }
 
 
+static sm_ret_t DisplaySettings_state(sm_t *me, sm_event_t const *e)
+{
+    switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("DisplaySettings_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("DisplaySettings_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    // case SM_INIT_SIG:
+    //     printf("DisplaySettings_state init\r\n");
+    //     return SM_TRAN(me, Brightness_state);
+    case SIG_DOWN:
+        printf("go NetworkSettings\r\n");
+        return SM_TRAN(me, NetworkSettings_state);
+    case SIG_ENTER_BRIGHTNESS:
+        printf("go Brightness\r\n");
+        return SM_TRAN(me, Brightness_state);
+    /* code */
+    }
+return SM_SUPER(me, Menu_state);
 
-//状态机初始化
-// void serial_sm_ctor(serial_hsm_t *me,sm_state_handler_t init)
-// {
-// hsm_ctor(&me->supper, init);
-// me->ringbuffer = NULL;
-// memset(me->frame, 0, sizeof(me->frame));
-// me->len = 0;
-// me->checksum = 0;
-// }
+
+}
+
+static sm_ret_t NetworkSettings_state(sm_t *me, sm_event_t const *e)
+{
 
 
+    switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("NetworkSettings_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("NetworkSettings_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    // case SM_INIT_SIG:
+    //     printf("NetworkSettings_state init\r\n");
+    //     return SM_TRAN(me, WIFI_state);
+    case SIG_UP:
+        printf("go DisplaySettings\r\n");
+        return SM_TRAN(me, DisplaySettings_state);
+    case SIG_ENTER_WIFI:
+        printf("go WIFI\r\n");
+        return SM_TRAN(me, WIFI_state);
+        
+    /* code */
+    }
+return SM_SUPER(me, Menu_state);
 
-    // ringbuf_t ringbuffer;
-    //     // 模拟环形缓冲区中的数据流
-    // uint8_t *ring_pool[256];
-    // uint8_t data_stream[] = {
-    //     0xE0, 0xB7,       // 头部
-    //     0x00, 0x06,       // 长度
-    //     0x01, 0x02, 0x03, // 数据负载
-    //     0x06,             // 校验和
-    //     0x00, 0xAA        // 尾部
-    // };
-    // serial_hsm_t serial_hsm;
+}
+
+static sm_ret_t Brightness_state(sm_t *me, sm_event_t const *e)
+{
+
+
+    switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("Brightness_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("Brightness_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SIG_DOWN:
+        printf("go Contrast_state\r\n");
+        return SM_TRAN(me, Contrast_state);
+        
+    /* code */
+    }
+return SM_SUPER(me, DisplaySettings_state);
+}
+
+static sm_ret_t Contrast_state(sm_t *me, sm_event_t const *e)
+{
+
+    switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("Contrast_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("Contrast_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SIG_UP:
+        printf("go Brightness_state\r\n");
+        return SM_TRAN(me, Brightness_state);
+        
+    /* code */
+    }
+return SM_SUPER(me, DisplaySettings_state);
+}
+
+static sm_ret_t WIFI_state(sm_t *me, sm_event_t const *e)
+{
+
+    switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("WIFI_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("WIFI_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SIG_DOWN:
+        printf("go Ethernet_state\r\n");
+        return SM_TRAN(me, Ethernet_state);
+        
+    /* code */
+    }
+return SM_SUPER(me, NetworkSettings_state);
+}
+
+static sm_ret_t Ethernet_state(sm_t *me, sm_event_t const *e)
+{
+
+        switch (e->sig)
+    {
+    case SM_ENTRY_SIG:
+        printf("Ethernet_state entry\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SM_EXIT_SIG:
+        printf("Ethernet_state exit\r\n");
+        return SM_HANDLED();  // 使用宏标记事件已处理
+    case SIG_UP:
+        printf("go WIFI_state\r\n");
+        return SM_TRAN(me, WIFI_state);
+    /* code */
+    }
+return SM_SUPER(me, NetworkSettings_state);
+}
+
 void init_fsm(void)
 {
+    hsm_ctor(&hsm,init);
+    hsm_init(&hsm,NULL);
 
-    hsm_ctor(&hsm, init);
-    hsm_init(&hsm, NULL);
-    elog_debug("hsm_init_finish");
-    // // 模拟事件驱动
-    sm_event_t event = { .sig = CHAR_SIG,0 };
 
+    sm_event_t event = { .sig = SIG_MENU,0 };
+
+    printf("sig_menu\r\n");
+    hsm_dispatch(&hsm, &event);//进入菜单
+
+    event.sig = SIG_ENTER_DISPLAY;//显示设置
+    printf("sig_enter\r\n");
     hsm_dispatch(&hsm, &event);
 
+    
+    event.sig = SIG_DOWN;//显示设置->网络设置
+    printf("sig_down\r\n");
+    hsm_dispatch(&hsm, &event);
+
+
+    event.sig = SIG_ENTER_WIFI;//wifi
+    printf("sig_enter\r\n");
+    hsm_dispatch(&hsm, &event);
+
+
+    event.sig = SIG_DOWN;// wifi->ethernet
+    printf("sig_down\r\n");
+    hsm_dispatch(&hsm, &event);//wifi->ethernet
+
+    event.sig = SIG_ESC;//回到主页
+    printf("sig_esc\r\n");
+    hsm_dispatch(&hsm, &event);//wifi->ethernet
     // sm_event_t event = { .sig = SM_USER_SIG,0 };
 
 
